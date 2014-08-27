90a6d161b8ad7d7ad3f177b2798787e4ba8908dd
Matt Earnshaw <matt@earnshaw.org.uk>
2014-08-27 18:26:04 +0100

    add include_package_data to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, find_packages
 setup(
         name = "labscript_utils",
         packages = find_packages(),
+        include_package_data = True,
         install_requires = ['h5py'] 
 )
 
